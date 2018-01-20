f9c29ade45ba2e6ce45cf5405b84cc2426c1e475
Alexis Poupelin <codevirtuel@gmail.com>
2018-01-20 20:00:17 +0100

    Added button to view and search replays

--- view.php
+++ view.php
@@ -1,21 +1,12 @@
 <!DOCTYPE html>
 <html>
-<?php
-	include("./class/VideoStream.php");
+	<head>
+		<link rel="stylesheet" type="text/css" href="css/view.css">
+	</head> 
 
-	//get the replay_id requested in the header
-	$replay_id = $_GET['id'];
-		if ($replay_id != -1) {
-			//Erreur
-		}
-		
-	$stream = new VideoStream("./replayList/".$replay_id."/".$replay_id.".mp4");
-	//$stream->start();
-?>
-<body>
-<video poster="/images/background.png" controls>
-	<source src="osu! 03-08-2017 12-45-17.avi"  type='video/mp4; codecs="h264"'>
-	<p>This is fallback content to display for user agents that do not support the video tag.</p>
-</video>
-</body>
+	<body>
+		<video poster="/images/background.png" controls>
+			<source src="osu! 03-08-2017 12-45-17.avi"  type='video/mp4; codecs="h264"'>
+		</video>
+	</body>
 </html>

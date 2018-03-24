141f1d689acc45b0eba1398f466466c16b5ce542
Poupelin Alexis <codevirtuel@gmail.com>
2018-03-24 23:42:05 +0100

    Added the core of registration
    - Show corresponding username of the id
    - Check if the email is valid
    - Check if both password match

---
+++ js/request.js
@@ -0,0 +1,85 @@
+//----------------------------- functions ----------------------------------
+function showUsername(str) {
+			if (str.length == 0) {
+				document.getElementById("txtHint").innerHTML = "";
+				return;
+			} else {
+				var xmlhttp = new XMLHttpRequest();
+				xmlhttp.onreadystatechange = function() {
+					if (this.readyState == 4 && this.status == 200) {
+						document.getElementById("txtHint").innerHTML = this.responseText;
+					}
+				};
+				xmlhttp.open("GET", "php/getUsername.php?q=" + str, true);
+				xmlhttp.send();
+			}
+		}
+
+function checkPassword() {
+	var firstPass = document.getElementById("pass").value;
+	var confirmPass = document.getElementById("confPass").value;
+	if(firstPass == confirmPass && firstPass != '' && confirmPass != ''){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+function showCheckPass(){
+	var firstPass = document.getElementById("pass").value;
+	var confirmPass = document.getElementById("confPass").value;
+	if(firstPass == confirmPass && firstPass != '' && confirmPass != ''){
+		document.getElementById("checkPass").innerHTML = "";
+	}else{
+		document.getElementById("checkPass").innerHTML = "The password doesn't match";
+	}
+}
+
+function hide(id){
+	node = document.getElementById(id);
+	if(node){
+		node.style.visibility = "hidden";
+	}
+}
+
+function show(id){
+	node = document.getElementById(id);
+	if(node){
+		node.style.visibility = "visible";
+	}
+}
+
+function validateEmail() {
+	var email = document.getElementById("email").value;
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+function showEmailValidity(){
+	if(validateEmail()){
+		document.getElementById("emailHint").innerHTML = "";
+	}else{
+		document.getElementById("emailHint").innerHTML = "This e-mail is not valid";
+	}
+}
+
+//----------------------------- At start ----------------------------------
+function start(){
+	hide("submitButton");
+	document.getElementById("email").value = '';
+}
+
+//----------------------------- At every update ---------------------------
+function update(){
+	//Check if the form is filled
+	if(checkPassword() && validateEmail()){
+		show("submitButton");
+	} else { hide("submitButton"); }
+}
+
+//----------------------------- When the form is submitted ----------------
+function submitted(){
+	
+}
+
+

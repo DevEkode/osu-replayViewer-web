c2a996774b279d97345187752d3f8b608946de6b
codevirtuel <codevirtuel@gmail.com>
2018-03-26 23:02:11 +0200

    Made some security improvements
    - Hide every keys into secure folder
    - Begining using mysql prepared statement

--- progress.php
+++ progress.php
@@ -2,12 +2,12 @@
 	ini_set('display_errors', 1);
 	//get replay the id
 	$replayId = $_GET['id'];
-	
+
 // ******************** Connection **********************************
 	// Connect to mysql request database --
-	$servername = "mysql.hostinger.fr";
-	$username = "u611457272_code";
-	require_once 'secure/mysql_pass.php';
+	require 'secure/mysql_pass.php';
+	$servername = $mySQLservername;
+	$username = $mySQLusername;
 	$password = $mySQLpassword;
 
 
@@ -20,7 +20,7 @@
 		header("Location:index.php?error=3");
 		exit;
 	}
-	
+
 //*************************** Variables ********************************
 //Max 75% - Min 5%
 //0 = In queue, 1 = Begin processing, 2 = Recording ,3 = Encoding, 4 = Uploading,
@@ -39,11 +39,11 @@ $statutD = array (
 			3 => "Encoding",
 			4 => "Uploading"
 		);
-		
+
 //*************************** Functions ********************************
 function getInfo($conn, $replayId, $column){
 	$result = $conn->query("SELECT * FROM requestlist WHERE replayId='$replayId'");
-	
+
 	if($result->num_rows > 0){
 		while($row = $result->fetch_assoc()){
 			$id = $row[$column];
@@ -54,7 +54,7 @@ function getInfo($conn, $replayId, $column){
 
 function getPlayerName($conn, $playerId){
 	$result = $conn->query("SELECT * FROM playerlist WHERE userId='$playerId'");
-	
+
 	if($result->num_rows > 0){
 		while($row = $result->fetch_assoc()){
 			$id = $row["userName"];
@@ -65,7 +65,7 @@ function getPlayerName($conn, $playerId){
 
 function checkProcessFinished($replayId,$conn){
 	$result = $conn->query("SELECT * FROM replaylist WHERE replayId='$replayId'");
-	
+
 	$bool = false;
 	if($result->num_rows > 0){
 		$bool = true;
@@ -104,24 +104,24 @@ if(checkProcessFinished($replayId,$conn)){
 
 	  gtag('config', 'UA-113523918-1');
 	</script>
-	
+
 	<title><?php echo $statutP[$statut]*100/75 ?>% - osu!replayViewer - A online osu replay viewer</title>
 	<link rel="stylesheet" type="text/css" href="css/progress.css">
 	<link rel="icon" type="image/png" href="images/icon.png" />
 	<meta http-equiv="refresh" content="<?php echo $sec?>;URL='<?php echo $page?>'">
-</head> 
+</head>
 
 <body>
-	
+
 	<section id="text">
 		<img src=<?php echo $url ?>>
 		<h3> <?php echo $username?> </h3>
-		
+
 		<h3><?php echo $BFN ?></h3>
 		<h3> <?php echo $statutP[$statut]*100/75 ?>% - Current state : <?php echo $statutD[$statut] ?> </h3>
 		<div id="bar" style="width:<?php echo $statutP[$statut]?>%"> </div>
 	</section>
-	
+
 	<footer>
 		osu!replayViewer is not affiliated with osu! - All credit to Dean Herbert
 		 | Website created by <a href="https://osu.ppy.sh/u/3481725">codevirtuel</a>
--- search.php
+++ search.php
@@ -1,21 +1,21 @@
 ﻿<!DOCTYPE html>
 <?php
-	ini_set('display_errors', 0);
-	
+	ini_set('display_errors', 1);
+
 	//********************* Variables **********************************
 	global $orderUpStars;
 	$orderUpStars = true;
 	$blockPerPages = 5;
-	
+
 	//-- Connect to mysql request database --
-		$servername = "mysql.hostinger.fr";
-		$usernameMysql = "u611457272_code";
-		require_once 'secure/mysql_pass.php';
-		$password = $mySQLpassword;
+	require 'secure/mysql_pass.php';
+	$servername = $mySQLservername;
+	$username = $mySQLusername;
+	$password = $mySQLpassword;
 
 	// ******************** Connection **********************************
 		// Create connection
-	    $conn = new mysqli($servername, $usernameMysql, $password, "u611457272_osu");
+	    $conn = new mysqli($servername, $username, $password, "u611457272_osu");
 
 		// Check connection
 		if ($conn->connect_error) {
@@ -23,25 +23,31 @@
 			header("Location:index.php?error=3");
 			exit;
 		}
-		
+
 		//--Connect to osu API --
 		require_once 'secure/osu_api_key.php';
 		$apiKey = $osuApiKey;
-		  
-		
+
+		//Prepared statements
+		$queryUserId = $conn->prepare("SELECT COUNT(*) AS nbr FROM replaylist WHERE userId=?");
+		$queryUserId->bind_param("i",$playerId);
+
+		$queryUserReplay = $conn->prepare("SELECT * FROM requestlist WHERE playerId=?");
+		$queryUserReplay->bind_param("i",$playerId);
+
 	// ******************** Functions **********************************
 	function getBeatmapJSON($beatmapId,$api){
 		$apiRequest = file_get_contents("https://osu.ppy.sh/api/get_beatmaps?k=$api&b=$beatmapId");
 		$json = json_decode($apiRequest, true);
 		return $json;
 	}
-	
+
 	function getUserJSON($username, $api){
 		$apiRequest = file_get_contents("https://osu.ppy.sh/api/get_user?k=$api&u=$username");
 		$json = json_decode($apiRequest, true);
 		return $json;
 	}
-		
+
 	function getUserId($api,$username){
 		//Query the playerId from osu api
 		$json = getUserJSON($username, $api);
@@ -51,7 +57,7 @@
 		}
 		return $json[0]['user_id'];
 	}
-	
+
 	function drawMods($bin){
 		$modsArray = array(1,2,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536,131072,262144,524288,1048576,2097152,4194304,16777216,33554432,67108864,134217728,268435456);
 		$modsName = array("NF","EZ","HD","HR","SD","DT","RL","HT","NC","FL","AT","SO","AP","PF","4K","5K","6K","7K","8K","FI","RD","CM","9K","COOP","1K","3K","2K");
@@ -59,14 +65,14 @@
 		if($bin != 0){
 			$string = "Mods : ";
 		}
-		
+
 		for($i=0;$i<count($modsArray)-1;$i++){
 			$result = $modsArray[$i] & $bin;
 			if($result != 0){
 				$string = $string.$modsName[$i].",";
 			}
 		}
-		
+
 		return substr($string, 0, -1);
 	}
 ?>
@@ -84,14 +90,14 @@
 
 		  gtag('config', 'UA-113523918-1');
 		</script>
-		
+
 		<link rel="stylesheet" type="text/css" href="css/search.css">
 		<meta charset="utf-8" />
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 		<link rel="icon" type="image/png" href="images/icon.png" />
-	</head> 
-	
+	</head>
+
 	<body>
 
 		<section id="form">
@@ -100,7 +106,7 @@
 				 <select name="choice">
 					<option value="username">username</option>
 					<option value="userId">user ID</option>
-				</select> 
+				</select>
 				<input type="text" name="playerId"/>
 				<input type="submit" name="SubmitButton"/>
 			</form>
@@ -110,7 +116,7 @@
 					1 => "This player doesn't exist",
 					2 => "This player id doesn't exist in the database"
 				);
-				
+
 				$error_id = isset($_GET['error']) ? (int)$_GET['error'] : 0;
 				if ($error_id != -1 && $error_id != 0) {
 					echo '<br>';
@@ -118,7 +124,7 @@
 				}
 			?>
 		</section>
-		
+
 		<!-- Result boxes -->
 		<?php
 		if(isset($_POST['SubmitButton']) || $_GET['u'] != 0){ //check if form was submitted
@@ -127,30 +133,27 @@
 			//form not submitted --> exit
 			goto end;
 		}
-		
+
 		//Get the post information
 		$playerId = $_POST['playerId'];
 		if($_POST['choice'] == "username"){
 			$playerId = getUserId($apiKey,$playerId);
 		}
-			
+
 		//Avoid SQL Injection
 		$playerId = intval($playerId);
-		
+
 		$inReplay = true;
 		$inRequest = true;
-		
+
 		//Calculation
-		$result = $conn->query("SELECT COUNT(*) AS nbr FROM replaylist WHERE userId=$playerId");
-			
-		if($result->num_rows > 0){
-			while($row = $result->fetch_assoc()){
-				$recordsNbr = $row['nbr'];
-			}
-		}
+		$queryUserId->execute();
+		$queryUserId->bind_result($recordsNbr);
+  	$row = $queryUserId->fetch();
+		$queryUserId->close();
 
 		$pageNbr = ceil($recordsNbr / $blockPerPages); //nbr of pages in total
-		
+
 		if(!isset($_GET['u']) && !isset($_GET["pn"]) && !isset($_GET['p'])){
 			header("Location:search.php?error=0&u=$playerId&pn=$pageNbr&p=0");
 		}else{
@@ -158,13 +161,13 @@
 			$pageNbr = $_GET["pn"];
 			$currentPage = $_GET["p"];
 		}
-		
+
 		//Query
 		if($playerId != 0){
-			
-			$result = $conn->query("SELECT * FROM requestlist WHERE playerId=$playerId");
-			if($result->num_rows > 0){
-				while($row = $result->fetch_assoc()){
+			$queryUserReplay->execute();
+			$queryUserReplay->store_result();
+			if($queryUserReplay->num_rows > 0){
+				while($row = $queryUserReplay->fetch()){
 					$beatmapSetId = $row['beatmapSetId'];
 					$beatmapId = $row['beatmapId'];
 					$beatmapName = base64_decode($row['BFN']);
@@ -181,11 +184,12 @@
 					echo	"<span></span>";
 					echo "</a>";
 				}
-			
+
 			}else{
 				$inRequest = false;
 			}
-			
+			$queryUserReplay->close();
+
 			if(orderUpStars == true){
 				$index = $currentPage * 5;
 				$result = $conn->query("SELECT * FROM replaylist WHERE userId=$playerId ORDER BY date DESC LIMIT $index, $blockPerPages");
@@ -207,7 +211,7 @@
 					$replayId = $row['replayId'];
 					$url = "https://b.ppy.sh/thumb/$beatmapSetId"."l.jpg";
 					$replayUrl = "http://osureplayviewer.xyz/view.php?id=$replayId";
-					
+
 					//play mod
 					switch($row['playMod']){
 						case 0 : $modUrl = "images/osuStdr.png"; break;
@@ -217,7 +221,7 @@
 						case 4 : $modUrl = ""; break;
 						default : $modUrl = ""; break;
 					}
-					
+
 					echo "<a class='content' href=$replayUrl>";
 					echo 	'<div id="anim">';
 					echo 		"<img src=$url>";
@@ -230,29 +234,29 @@
 					echo	"<span></span>";
 					echo "</a>";
 				}
-			
+
 			}else{
 				$inReplay = false;
 			}
-			
+
 			if(!$inReplay && !$inRequest){
 				header("Location:search.php?error=2");
 				exit;
 			}
 		}
-		
+
 		if($pageNbr > 1){
 			$index = $currentPage+1;
 			echo	'<div class="div-align">';
-			
+
 			if($currentPage != 0){
 				$previous = $currentPage-1;
 				$url = "./search.php?error=0&u=$playerId&pn=$pageNbr&p=$previous";
 				echo	"<a href=$url title=\"Previous page\"><img class=\"img-valign\" src=\"images/arrow_button_left.png\" alt=\"\"/></a>";
 			}
-			
+
 			echo	"<span> $index / $pageNbr </span>";
-				
+
 			if($currentPage != $pageNbr-1){
 				$next = $currentPage+1;
 				$url = "./search.php?error=0&u=$playerId&pn=$pageNbr&p=$next";
@@ -260,15 +264,15 @@
 			}
 			echo	'</div>';
 		}
-		
+
 		end:
-		?> 
-		
+		?>
+
 		<footer>
 			osu!replayViewer is not affiliated with osu! - All credit to Dean Herbert
 			| Website created by <a href="https://osu.ppy.sh/u/3481725">codevirtuel</a>
 		</footer>
 	</body>
-	
-	
+
+
 </html>

0cff505fec07d1fab86d0e3fde1b26d41e5876ea
codevirtuel <codevirtuel@gmail.com>
2018-04-12 23:14:17 +0200

    Making progress on the profile page

--- userProfile.php
+++ userProfile.php
@@ -1,10 +1,77 @@
 <?php
 session_start();
 
-//get the session info
-if(empty($_SESSION)) header("Location:index.php");
-$userId = $_SESSION["userId"];
+//-------- Connect to mysql request database ---------
+require 'secure/mysql_pass.php';
+$servername = $mySQLservername;
+$username = $mySQLusername;
+$password = $mySQLpassword;
 
-$osuProfileLink = "https://osu.ppy.sh/users/"+$userId;
+$conn = new mysqli($servername, $username, $password, "u611457272_osu");
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+	header("Location:index.php?error=3");
+	exit;
+}
+
+//***************** Functions **********************
+function getDBInfo($conn,$userId){
+  $query = $conn->prepare("SELECT * FROM accounts WHERE userId=?");
+  $query->bind_param("i",$userId);
+  $query->execute();
+  $result=$query->get_result();
+
+  while($row=$result->fetch_assoc()){
+    $username = $row['username'];
+  }
+  return $username;
+}
+
+//get the session info
+$isLogged = false;
+$username = "unknown";
+
+if(!empty($_SESSION)){
+  $isLogged=true;
+  $userId = $_SESSION["userId"];
+} else{
+  if(!isset($_GET['id'])) header("Location:index.php");
+  $userId=$_GET['id'];
+}
+
+//check if the player exist un DB
+$username=getDBInfo($conn,$userId);
+if(empty($username)){
+  header("Location:index.php");
+}
+
+$osuProfileLink = "https://osu.ppy.sh/users/".$userId;
 $profileImg = "images/defaultProfilePicture.png";
  ?>
+
+<html>
+  <head>
+    <title>osu!replayViewer - unknown profile</title>
+    <link rel="stylesheet" type="text/css" href="css/userProfile.css">
+    <link rel="icon" type="image/png" href="images/icon.png" />
+  </head>
+
+  <body>
+    <block id="header" class="block">
+      <img src="images/defaultProfilePicture.png" id="userImage">
+      <div id="headerContent">
+        <h1> <?php echo $username; ?> </h1>
+        <h3> <?php echo $userId; ?> </h3>
+
+      </div>
+        <a href=<?php echo $osuProfileLink ?> id="osuImage"><img src="images/osu_logo.png"></a>
+    </block>
+
+    <block id="replayList" class="block">
+      <h2> Replay library</h2>
+    </block>
+  </body>
+
+
+</html>

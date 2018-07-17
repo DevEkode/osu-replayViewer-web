018600932a1f20d0aa967c8a57beea8595f6ff7f
Alexis Poupelin <codevirtuel@gmail.com>
2018-07-17 14:55:07 +0200

    Added modal screen

---
+++ js/index/upload.js
@@ -0,0 +1,11 @@
+//modal
+var modal;
+
+function onChange(){
+  modal = document.getElementById("myModal");
+  modal.style.display = "block";
+}
+
+function closeModal(){
+  modal.style.display = "none";
+}

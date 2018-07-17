26fc4e77f61ccf8b1d578377b3e20d493b922080
Alexis Poupelin <codevirtuel@gmail.com>
2018-07-17 22:22:56 +0200

    Finished modal

--- js/index/upload.js
+++ js/index/upload.js
@@ -1,7 +1,7 @@
 //modal
 var modal;
 
-function onChange(){
+function openModal(){
   modal = document.getElementById("myModal");
   modal.style.display = "block";
 }
@@ -9,4 +9,21 @@ function onChange(){
 function closeModal(){
   modal = document.getElementById("myModal");
   modal.style.display = "none";
+}
+
+function submitForm(){
+  document.getElementById("upload_box").submit();
+}
+
+function setItemTrue(item){
+  document.getElementById(item).style["border-color"] = "lightgreen";
+}
+
+function setItemFalse(item){
+  document.getElementById(item).style["border-color"] = "red";
+}
+
+function disableProcessing(){
+  document.getElementById("start_processing").disabled = true;
+  document.getElementById("checkBox").disabled = true;
 }

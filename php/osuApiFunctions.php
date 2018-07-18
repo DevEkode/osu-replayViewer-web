ad36c295d01bde3bc65a5d7d581ad2bdc3424e23
Alexis Poupelin <codevirtuel@gmail.com>
2018-07-18 20:01:58 +0200

    Finished new search page design

--- php/osuApiFunctions.php
+++ php/osuApiFunctions.php
@@ -33,6 +33,17 @@
 		return json_decode($result, true);
 	}
 
+  function getReplayJSON($beatmapId,$user,$gamemode,$mods,$api){
+    $url = "https://osu.ppy.sh/api/get_scores?k=$api&b=$beatmapId&u=$user&m=$gamemode&mods=$mods";
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_URL,$url);
+		$result=curl_exec($ch);
+		curl_close($ch);
+		return json_decode($result, true);
+  }
+
   //Return mods name from the binary (divided by ' ')
   function drawMods($bin){
 		$modsArray = array(1,2,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536,131072,262144,524288,1048576,2097152,4194304,16777216,33554432,67108864,134217728,268435456);
@@ -111,5 +122,6 @@
 
   	return $BFN;
   }
+
 
  ?>
---
+++ php/search/blockModel.php
@@ -0,0 +1,60 @@
+<?php
+
+function drawProfile($userId,$username){
+  $profileURL = "userProfile.php?id=".$userId;
+  $userImgURL = "https://a.ppy.sh/".$userId;
+  echo "<a class='requestContent' href=$profileURL>";
+  echo 	'<div id="anim">';
+  echo 		"<img src=$userImgURL>";
+  echo 	'</div>';
+  echo	"<h3 id=\"beatmap_name\">".$username."</h3>";
+  echo 	"<h4>Click here to visit his profile</h4>";
+  echo	"<span></span>";
+  echo'</a>';
+}
+
+function drawRequest($replayId,$beatmapName){
+  $beatmapName = str_replace(".osz", "", $beatmapName);
+  $url = "https://b.ppy.sh/thumb/$beatmapSetId"."l.jpg";
+  $replayUrl = "progress.php?id=$replayId";
+  echo "<a class='requestContent' href=$replayUrl>";
+  echo 	'<div id="anim">';
+  echo 		"<img src=$url>";
+  echo 	'</div>';
+  echo	"<h3>$beatmapName</h3>";
+  echo 	"<h4>Currently in processing queue</h4>";
+  echo	"<span></span>";
+  echo "</a>";
+}
+
+function drawReplay($replayId,$beatmapName,$beatmapSetId,$artist,$diff,$gamemode,$modsListing){
+
+  //gamemode
+  switch($gamemode){
+    case 0 : $modUrl = "images/osuStdr.png"; break;
+    case 1 : $modUrl = "images/osuTaiko.png"; break;
+    case 2 : $modUrl = "images/osuCTB.png"; break;
+    case 3 : $modUrl = "images/osuMania.png"; break;
+    case 4 : $modUrl = ""; break;
+    default : $modUrl = ""; break;
+  }
+
+  $url = "https://b.ppy.sh/thumb/$beatmapSetId"."l.jpg";
+  $replayUrl = "view.php?id=$replayId";
+
+  echo "<a class='content' href=$replayUrl>";
+  echo 	'<div id="anim">';
+  echo 		"<img src=$url>";
+  echo 	'</div>';
+  echo	'<div id="alignRight">';
+  echo		"<img src=$modUrl>";
+  echo	'</div>';
+  echo	"<h3 id=\"beatmap_name\">$beatmapName</h3>";
+  echo 	"<h4 id=\"block_content\">Creator : $artist <br>
+        Difficulty : $diff <br>
+        Mods : $modsListing</h4>";
+  echo	"<span></span>";
+  echo "</a>";
+}
+
+ ?>

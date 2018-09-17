53e9eaf81054a84042b53ac51f8941fbb268b148
codevirtuel <codevirtuel@gmail.com>
2018-09-17 20:09:50 +0200

    New error messages

---
+++ php/errors.php
@@ -0,0 +1,11 @@
+<?php
+$uploadErrors = array(
+  1 => 'Mysql connection error',
+  2 => 'Cannot get replay data',
+  3 => 'Cannot get beatmap data',
+  4 => 'Cannot get player data',
+  5 => 'INSERT INTO mysql database failed',
+  6 => 'Cannot create replay directory',
+  7 => 'Cannot move replay to final destination'
+);
+ ?>

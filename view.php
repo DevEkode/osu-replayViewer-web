89a8870c76289488096231baeda9a28b93823333
codevirtuel <codevirtuel@gmail.com>
2018-11-15 23:31:06 +0100

    Added disqus on view page #17

--- view.php
+++ view.php
@@ -240,7 +240,6 @@
     ?>
 
     <br>
-
     <div class="second_block">
       <span id="section_title">Share</span>
       <div id="share_section">
@@ -255,6 +254,30 @@
         <iframe src=<?php echo $facebookURL; ?> width="91" height="28" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
       </div>
     </div>
+
+    <br>
+
+    <!-- Disqus -->
+    <div id="disqus_thread"></div>
+      <script>
+
+      /**
+      *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+      *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+      /*
+      var disqus_config = function () {
+      this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+      this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+      };
+      */
+      (function() { // DON'T EDIT BELOW THIS LINE
+      var d = document, s = d.createElement('script');
+      s.src = 'https://osureplayviewer.disqus.com/embed.js';
+      s.setAttribute('data-timestamp', +new Date());
+      (d.head || d.body).appendChild(s);
+      })();
+      </script>
+      <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
 
     <!-- Footer -->
     <footer>
@@ -278,6 +301,7 @@
         <span> website created by codevirtuel <a href="https://osu.ppy.sh/u/3481725" target="_blank"><img src="images/codevirtuel.jpg"/></a></span>
       </div>
     </footer>
+    <script id="dsq-count-scr" src="//osureplayviewer.disqus.com/count.js" async></script>
   </body>
 
 </html>

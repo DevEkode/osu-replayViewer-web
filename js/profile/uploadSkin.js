cd492daea52c529c0f10da1e70e33f5f47910bc9
Poupelin Alexis <codevirtuel@gmail.com>
2019-02-11 16:45:52 +0100

    Forgot some urls updates

--- js/profile/uploadSkin.js
+++ js/profile/uploadSkin.js
@@ -4,7 +4,7 @@ var max =  52428800; // 50Mo
 if (file.size > max) {
     var pathname = window.location.pathname;
     removeHeaders = false;
-    window.location.replace(pathname+"?skinError=5");
+    window.location.replace(pathname+"?block=skin&error=11");
     document.getElementById("fileToUpload").value = null; // Clear the field.
   }else{
     return true;
@@ -16,7 +16,7 @@ function checkFileCharacters(file){
   if(patt.test(file)){
     var pathname = window.location.pathname;
     removeHeaders = false;
-    window.location.replace(pathname+"?skinError=4");
+    window.location.replace(pathname+"?block=skin&error=10");
     document.getElementById("fileToUpload").value = null; // Clear the field.
   }else{
     return true;

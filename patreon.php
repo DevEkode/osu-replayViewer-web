6530bd9e0cf5495431ce4a22467944f9716b9614
Alexis Poupelin <codevirtuel@gmail.com>
2019-03-12 19:49:39 +0100

    Finished patreon page

--- patreon.php
+++ patreon.php
@@ -1,10 +1,10 @@
 <?php
-    session_start();
-    require 'php/navbar.php';
+session_start();
+require 'php/navbar.php';
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
     <!-- Metadatas -->
@@ -23,13 +23,17 @@
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-134700452-1"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
-        function gtag() { dataLayer.push(arguments); }
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
         gtag('js', new Date());
         gtag('config', 'UA-134700452-1');
     </script>
 
     <title>osu!replayViewer - Patreon supporters page</title>
-    <link rel="icon" type="image/png" href="images/icon.png" />
+    <link rel="icon" type="image/png" href="images/icon.png"/>
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 
     <!-- libraries -->
@@ -44,47 +48,30 @@
 
     <!-- javascript -->
     <script src="js/loader.js"></script>
+    <script src="js/patreon/loadBlocks.js"></script>
 
     <!-- Cookie bar -->
     <script type="text/javascript"
-        src="https://cdn.jsdelivr.net/npm/cookie-bar/cookiebar-latest.min.js?theme=flying&tracking=1&always=1&scrolling=1"></script>
+            src="https://cdn.jsdelivr.net/npm/cookie-bar/cookiebar-latest.min.js?theme=flying&tracking=1&always=1&scrolling=1"></script>
 </head>
 
 <body>
-    <!-- Top navigation bar -->
-    <?php showNavbar(); ?>
+<!-- Top navigation bar -->
+<?php showNavbar(); ?>
 
-    <!-- presentation -->
-    <h1 id="title">Patreon supporters</h1>
+<!-- presentation -->
+<h1 id="title">Patreon supporters</h1>
 
-    <h2 id="slogan">Help me build this project !</h2>
+<h2 id="slogan">Help me build this project !</h2>
 
-    <div style="margin-left:5%; margin-right:5%">
-    <div class="container-fluid" id="tier_block">
-        <div class="row">
-            <div class="col-xs-9 col-sm-9 col-md-9">
-                <h3><b>Tier Name</b>
-                <small class="text-muted">- X pledgers</small>
-                </h3>
-            </div>
-            <div class="col-xs-3 col-sm-3 col-md-3" id="join_block_button">
-                <button type="button" class="btn btn-danger">Join for 1$ !</button>
-            </div>
-        </div>
-        <div class="row">
-            </p>
-            <div class="col-xs-9 col-sm-9 col-md-9">
-                <h4>Thank you ❤️,<h4>
-                <p class="lead">
-                <small class="text-muted">Name1, Name2, Name3, ...</small>
-            </div>
-            <div class="col-xs-3 col-sm-3 col-md-3">
-                <small class="text-muted">View all the benefits on the patreon page</small>
-            </div>
-        </div>
-    </div>
+<div class="align_center" style="margin-bottom: 1%">
+    <a href="https://www.patreon.com/bePatron?u=17710775" data-patreon-widget-type="become-patron-button">Become a Patron!</a><script async src="https://c6.patreon.com/becomePatronButton.bundle.js"></script>
 </div>
 
-    <div class="loaderCustom"></div>
-    <?php showFooter() ?>
+<div id="tier_container" style="margin-left:5%; margin-right:5%">
+
+</div>
+
+<div class="loaderCustom"></div>
+<?php showFooter() ?>
 </body>
--- php/patreon/getPledgers.php
+++ php/patreon/getPledgers.php
@@ -8,63 +8,38 @@
 require_once '../../vendor/autoload.php';
 require_once '../../secure/patreon_key.php';
 
-use Patreon\API;
-use Patreon\OAuth;
+//Get pledgers names
+$files = array(
+    0 => '1_pledgers.txt',
+    1 => '5_pledgers.txt'
+);
 
-$api_client = new API($patreon_access_token);
-$current_campaign = $api_client->fetch_campaign();
-$current_patreons = $api_client->fetch_page_of_pledges(2487929,5);
+//Creating URL request
 
+$url = 'https://www.patreon.com/api/oauth2/api/current_user/campaigns?access_token='.$patreon_access_token;
 
-$tiers = array(3,4);
-$tiers_temp = array();
+//Get response json
+$url_content = file_get_contents($url);
+$json = json_decode($url_content);
 
-//Get tiers data
-foreach($tiers as &$tier_id){
-    $tiers_temp[$tier_id] = $current_campaign->get('included')->get($tier_id);
-}
-$tiers = $tiers_temp;
+//Get rewards
+$rewards = array();
+$index = 0;
+foreach($json->included as $data){
+    //Check if the data is a reward
+    if(strcmp($data->type,'reward') == 0){
+        if($data->id == -1 || $data->id == 0) continue;
+        $newData = $data->attributes;
 
-//Get pledgers
-$pledgers_data = $current_patreons->get('data');
-$pledgers = array();
-foreach($pledgers_data->getKeys() as $key=>$value){
-    $pledger_temp = $pledgers_data->get($key);
+        //Add pledgers names
+        $pledgers = explode(";",file_get_contents('./'.$files[$index]));
+        $newData->pledgers = $pledgers;
 
-    $user = array();
-    $user['id'] = $pledger_temp->get('id');
-
-    $user_info = $api_client->fetch_user($user['id'])->get('data');
-
-    $user['name'] = $user_info->get('attributes')->get('full_name');
-
-    $pledgers[$key] = $user;
-}
-
-var_dump($pledgers);
-
-//Construct json
-$json_model = array();
-$final_json = array();
-
-foreach ($tiers as $key=>$tiers_obj){
-    $json_temp = $json_model;
-
-    $json_temp['id'] = $tiers_obj->get('id');
-    $json_temp['name'] = $tiers_obj->get('attributes')->get('title');
-    $json_temp['url'] = $tiers_obj->get('attributes')->get('url');
-    $json_temp['price_cents'] = $tiers_obj->get('attributes')->get('amount_cents');
-    $json_temp['remaining'] = $tiers_obj->get('attributes')->get('remaining');
-    $json_temp['pledgers_nbr'] = $tiers_obj->get('attributes')->get('patron_count');
-
-    //Find pledgers for this tier
-
-
-    $final_json[$key] = $json_temp;
+        array_push($rewards,$newData);
+        $index++;
+    }
 }
 
-
-
-
-var_dump($final_json);
+header('Content-type: application/json');
+echo json_encode($rewards);
 ?>

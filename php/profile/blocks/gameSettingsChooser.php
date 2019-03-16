a6ae712e83a3280d1f632570f4e7a7040068887a
Alexis Poupelin <codevirtuel@gmail.com>
2019-03-16 11:53:36 +0100

    Fixed typo to match osu! settings names

--- php/profile/blocks/gameSettingsChooser.php
+++ php/profile/blocks/gameSettingsChooser.php
@@ -19,7 +19,7 @@ generateSwitch("Combo bursts","combo_bursts","combo_bursts");
 generateSwitch("Hit lighting","hit_lighting","hit_lighting");
 generateSwitch("Replay HUD","replay_hud","replay_hud");
 generateSwitch("Spectator HUD","spec_hud","spec_hud");
-generateSwitch("Beatmap skins","beatmap_skin","beatmap_skin");
+generateSwitch("Ignore beatmap skins","beatmap_skin","beatmap_skin");
 
 echo '</div>';
 echo '<input type="submit" value="Save all modifications" class="button is-light"/>';

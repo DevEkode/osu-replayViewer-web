026dfb2232906b93f822a5dd6266b997a999f42d
Ekode <ekode@protonmail.com>
2019-09-10 15:07:11 +0200

    Added checkbox for multiple selection

--- editProfile.php
+++ editProfile.php
@@ -1,32 +1,32 @@
 <?php
-  session_start();
-  require 'php/errors.php';
-  require 'php/success.php';
-  require 'php/navbar.php';
+session_start();
+require 'php/errors.php';
+require 'php/success.php';
+require 'php/navbar.php';
 
-  require 'php/profile/blockManager.php';
+require 'php/profile/blockManager.php';
 
-  if(empty($_SESSION['userId'])){
+if (empty($_SESSION['userId'])) {
     header("Location:index.php");
-  }
+}
 
-  require 'php/profile/replaySettings.php';
+require 'php/profile/replaySettings.php';
 
-  //Query user information
-  checkUserFile($_SESSION["userId"]);
-  /*$skins = listAllSkins($_SESSION["userId"]);
-  $customSkin = getIniKey($_SESSION["userId"],,"enable");
-  $actualSkin = getIniKey($_SESSION["userId"],"fileName");
-  $actualDim = getIniKey($_SESSION["userId"],"dim");
-  $actualCursorSize = getIniKey($_SESSION["userId"],"cursor_size");*/
- ?>
+//Query user information
+checkUserFile($_SESSION["userId"]);
+/*$skins = listAllSkins($_SESSION["userId"]);
+$customSkin = getIniKey($_SESSION["userId"],,"enable");
+$actualSkin = getIniKey($_SESSION["userId"],"fileName");
+$actualDim = getIniKey($_SESSION["userId"],"dim");
+$actualCursorSize = getIniKey($_SESSION["userId"],"cursor_size");*/
+?>
 
 <!DOCTYPE html>
 <html>
-  <head>
+<head>
     <script src="js/request.js"></script>
     <script src="js/editProfile.js"></script>
-    
+
     <title>osu!replayViewer - edit profile</title>
     <link rel="stylesheet" type="text/css" href="css/navbar.css">
     <link rel="stylesheet" type="text/css" href="css/footer.css">
@@ -34,19 +34,26 @@
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-134700452-1"></script>
     <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
+        window.dataLayer = window.dataLayer || [];
 
-      gtag('config', 'UA-134700452-1');
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
+        gtag('js', new Date());
+
+        gtag('config', 'UA-134700452-1');
     </script>
 
 
-      <link rel="icon" type="image/png" href="images/icon.png"/>
+    <link rel="icon" type="image/png" href="images/icon.png"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="node_modules/bulma/css/bulma.css">
-      <link rel="stylesheet" type="text/css" href="node_modules/bulma-tooltip/dist/css/bulma-tooltip.min.css">
+    <link rel="stylesheet" type="text/css" href="node_modules/bulma-tooltip/dist/css/bulma-tooltip.min.css">
+    <link rel="stylesheet" type="text/css"
+          href="node_modules/cool-checkboxes-for-bulma.io/dist/css/bulma-radio-checkbox.min.css">
     <link rel="stylesheet" type="text/css" href="css/editProfile.css">
-    
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.js"></script>
     <script src="js/loader.js"></script>
@@ -56,54 +63,60 @@
     <script src="js/profile/volume.js"></script>
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <!-- Cookie bar -->
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/cookie-bar/cookiebar-latest.min.js?theme=flying&tracking=1&always=1&scrolling=1"></script>
+    <script type="text/javascript"
+            src="https://cdn.jsdelivr.net/npm/cookie-bar/cookiebar-latest.min.js?theme=flying&tracking=1&always=1&scrolling=1"></script>
 
     <link rel="stylesheet" type="text/css" href="css/loader.css">
     <link rel="stylesheet" type="text/css" href="css/checkboxSwitch.css">
-  </head>
+</head>
 
-  <body onload="showDim(); showCursorSize(); updateCustomSkin(); updateVolume();">
-    <div class="loaderCustom"></div>
-    <?php showNavbar(); ?>
-    <?php showError(); ?>
+<body onload="showDim(); showCursorSize(); updateCustomSkin(); updateVolume();">
+<div class="loaderCustom"></div>
+<?php showNavbar(); ?>
+<?php showError(); ?>
 
-    <!-- Modal -->
-    <div class="modal" id="delete_modal">
-      <div class="modal-content">
+<!-- Modal -->
+<div class="modal" id="delete_modal">
+    <div class="modal-content">
         <h2>Do you really want to delete your account ?</h2>
         <h4>A verification email will be send if you accept</h4>
         <form action="php/profile/deleteProfile.php" method="post">
-          <input type="submit" id="button_yes" value="Yes please !"/>
-          <input type="hidden" name="id" value=<?php echo $_SESSION['userId']; ?> />
+            <input type="submit" id="button_yes" value="Yes please !"/>
+            <input type="hidden" name="id" value=<?php echo $_SESSION['userId']; ?>/>
         </form>
         <button id="button_no" onclick="closeModalDelete()">No stop !</button>
-      </div>
     </div>
+</div>
 
-    <h1 id="TopTitle"> Edit profile </h1>
+<h1 id="TopTitle"> Edit profile </h1>
 
-    <div class="block" id="replay">
+<div class="block" id="replay">
 
-        <div class="columns is-desktop">
-            <div class="column is-one-quarter-desktop is-full-tablet" id="columnBack">
-          <!-- First column -->
-          <?php generateMenu(); ?>
+    <div class="columns is-desktop">
+        <div class="column is-one-quarter-desktop is-full-tablet" id="columnBack">
+            <!-- First column -->
+            <?php generateMenu(); ?>
         </div>
         <div class="column">
-          <!-- Second column -->
-          <?php generateBlocks(); ?>
+            <!-- Second column -->
+            <?php generateBlocks(); ?>
             <div class="columns is-desktop is-multiline">
                 <div class="column is-4-fullhd is-6-desktop is-12-tablet">
                     <div class="card grow">
                         <div class="card-image">
-                            <figure class="image is-4by2">
+                            <figure class="image is-4by2 container_check">
                                 <img src="images/preview.jpg" alt="Placeholder image">
+                                <div class="b-checkbox checkbox_card">
+                                    <input id="checkbox" class="styled" type="checkbox">
+                                    <label for="checkbox">
+                                    </label>
+                                </div>
                             </figure>
                         </div>
                         <div class="card-content">
                             <div class="media">
                                 <div class="media-left">
-                                    <figure class="image is-48x48">
+                                    <figure class="image is-48x48 tooltip" data-tooltip="Osu!CTB">
                                         <img class="is-rounded has-background-grey-dark" src="images/osuCTB.png"
                                              alt="Placeholder image">
                                     </figure>
@@ -130,13 +143,13 @@
                 </div>
             </div>
         </div>
-      </div>
     </div>
+</div>
 
-    <div class="spacer">
-      <br>
-    </div>
+<div class="spacer">
+    <br>
+</div>
 
-    <?php showFooter() ?>
-  </body>
+<?php showFooter() ?>
+</body>
 </html>

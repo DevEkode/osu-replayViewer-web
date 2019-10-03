f02a55e0584eeb02d957d68fb0b8a4a49b36fe68
Ekode <ekode@protonmail.com>
2019-10-03 20:06:22 +0200

    Added Mirror, TargetPractice and ScoreV2 Mods
    # Conflicts:
    #	php/osuApiFunctions.php

--- php/osuApiFunctions.php
+++ php/osuApiFunctions.php
@@ -47,15 +47,15 @@
 
   //Return mods name from the binary (divided by ' ')
   function drawMods($bin){
-		$modsArray = array(1,2,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536,131072,262144,524288,1048576,2097152,4194304,16777216,33554432,67108864,134217728,268435456);
-		$modsName = array("NF","EZ","HD","HR","SD","DT","RL","HT","NC","FL","AT","SO","AP","PF","4K","5K","6K","7K","8K","FI","RD","CM","9K","COOP","1K","3K","2K");
+    $modsArray = array(1, 2, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304,8388608, 16777216, 33554432, 67108864, 134217728, 268435456, 536870912, 1073741824);
+    $modsName = array("NF", "EZ", "HD", "HR", "SD", "DT", "RL", "HT", "NC", "FL", "AT", "SO", "AP", "PF", "4K", "5K", "6K", "7K", "8K", "FI", "RD", "CM","TP", "9K", "COOP", "1K", "3K", "2K", "ScoreV2", "Mirror");
 		if($bin != 0){
 			$string = "";
 		}else{
       $string = "none";
     }
 
-		for($i=0;$i<count($modsArray)-1;$i++){
+    for ($i = 0; $i < count($modsArray); $i++) {
 			$result = $modsArray[$i] & $bin;
 			if($result != 0){
 				$string = $string.$modsName[$i]." ";
@@ -71,8 +71,8 @@
 
 function getModsArray($bin)
 {
-    $modsArray = array(1, 2, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304, 16777216, 33554432, 67108864, 134217728, 268435456);
-		$modsName = array("NF","EZ","HD","HR","SD","DT","RL","HT","NC","FL","AT","SO","AP","PF","4K","5K","6K","7K","8K","FI","RD","CM","9K","COOP","1K","3K","2K");
+    $modsArray = array(1, 2, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304,8388608, 16777216, 33554432, 67108864, 134217728, 268435456, 536870912, 1073741824);
+    $modsName = array("NF", "EZ", "HD", "HR", "SD", "DT", "RL", "HT", "NC", "FL", "AT", "SO", "AP", "PF", "4K", "5K", "6K", "7K", "8K", "FI", "RD", "CM","TP", "9K", "COOP", "1K", "3K", "2K", "ScoreV2", "Mirror");
 		$resultArray = array();
 		if($bin != 0){
 			$resultArray = array();
@@ -175,7 +175,7 @@ function isBeatmapAvailable($beatmapId, $key)
     }
 
     //Check mods
-    if($replayDATA['Mods'] > 268435456){
+    if ($replayDATA['Mods'] > 1073741824)) {
       echo 'Mods not valid <br>';
       $valide = false;
     }

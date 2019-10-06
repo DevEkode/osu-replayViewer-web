21722f2aada6b0b9d4ea3e48915485589a6dbd94
Ekode <ekode@protonmail.com>
2019-10-06 19:17:45 +0200

    Added more cron task

--- cron/autoAccountDelete.php
+++ cron/autoAccountDelete.php
@@ -1,22 +1,6 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/startup.php';
 
-//-- Connect to mysql request database --
-
-//Time limit in days
-$timeLimit = 1;
-
-// ******************** Connection **********************************
-// Create connection
-$conn = new mysqli(getenv('MYSQL_HOST'), getenv('MYSQL_HOST'), getenv('MYSQL_PASS'), getenv('MYSQL_DB'));
-//$conn = new PDO('mysql:host=mysql.hostinger.fr;dbname=u611457272_osu','u611457272_code','123');
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-	exit;
-}
-
 function getRemovableAccounts($conn,$timeLimit){
   $array = array();
 
@@ -55,17 +39,5 @@ function removeFolder($dir){
 	cleanFolder($dir);
 	//delete folder
 	rmdir($dir);
-}
-
-echo "==== Deleting account with register time expired ===";
-
-$accountsToRemove = getRemovableAccounts($conn,$timeLimit);
-if(!empty($accountsToRemove)){
-  foreach($accountsToRemove as $userId){
-    deleteAccount($conn,$userId);
-    echo 'Deleted account with id '.$userId.' <br>';
-  }
-}else{
-  echo 'No account to delete';
 }
  ?>
--- cron/cron.php
+++ cron/cron.php
@@ -1,6 +1,32 @@
 <?php
-//Regroup every cron file for execution
+require_once $_SERVER['DOCUMENT_ROOT'] . '/startup.php';
+include_once './autoAccountDelete.php';
+include_once 'ReplayCompressor.php';
 
-include './autoAccountDelete.php';
+//-- Regroup every cron file for execution --
+
+//Time limit in days
+$timeLimit = 1;
+
+// ******************** Connection **********************************
+// Create connection
+$conn = new mysqli(getenv('MYSQL_HOST'), getenv('MYSQL_HOST'), getenv('MYSQL_PASS'), getenv('MYSQL_DB'));
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "==== Deleting account with register time expired ===";
+
+$accountsToRemove = getRemovableAccounts($conn,$timeLimit);
+if(!empty($accountsToRemove)){
+    foreach($accountsToRemove as $userId){
+        deleteAccount($conn,$userId);
+        echo 'Deleted account with id '.$userId.' <br>';
+    }
+}else{
+    echo 'No account to delete';
+}
 
 ?>
